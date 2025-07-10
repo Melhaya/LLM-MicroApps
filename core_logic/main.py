9db80464618fcc0c4e241f114ba244ce1c8d9a37
Melhaya <mohamed.osama211@gmail.com>
2025-07-10 15:13:49 +0200

    remove fix for connection to github
    didn't work. nothing happens

--- core_logic/main.py
+++ core_logic/main.py
@@ -408,19 +408,6 @@ def main(config):
     PREFERRED_LLM = config.get('PREFERRED_LLM', 'openai')
     SYSTEM_PROMPT = config.get('SYSTEM_PROMPT', '')
 
-    st.markdown(
-        """
-        <style>
-        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
     # Apply the page configuration
     if PAGE_CONFIG:
         st.set_page_config(
